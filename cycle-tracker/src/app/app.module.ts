309300ef7292c3dd475e114ad7cd2868830185a3
kunklaa <79965712+kunklaa@users.noreply.github.com>
2022-08-21 19:52:43 +0200

    added firebase, auth service, dummy data calendar

--- cycle-tracker/src/app/app.module.ts
+++ cycle-tracker/src/app/app.module.ts
@@ -6,10 +6,23 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { AngularFireModule } from '@angular/fire/compat'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase), 
+    //provideFirebaseApp(() => initializeApp(environment.firebase)), 
+    provideAuth(() => getAuth()), 
+    provideFirestore(() => getFirestore()), 
+    provideStorage(() => getStorage())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- cycle-tracker/src/app/pages/calendar/calendar.page.ts
+++ cycle-tracker/src/app/pages/calendar/calendar.page.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarPage implements OnInit {
 
+  selectedDate = "2022-08-10";
+  selectedSymptoms;
+
+  dummySymptoms = {
+    "2022-08-10" : {
+      moods: ["Jó", "Rossz", "Ideges", "Kedvtelen", "Nyugodt"],
+      temperature: 36.4,
+      blood: "Erős vérzés"
+    },
+    "2022-08-11" : {
+      moods: ["Jó"],
+      temperature: 35.6,
+      blood: "Közepes vérzés"
+    },
+    "2022-08-13" : {
+      moods: ["Rossz", "Ideges"],
+      temperature: 36.0,
+      blood: "Gyenge vérzés"
+    },
+    "2022-08-14" : {
+      moods: ["Kedvtelen", "Nyugodt"],
+      temperature: 37.0,
+      blood: "Nincs vérzés"
+    },
+  }
+
   constructor() { }
 
   ngOnInit() {
+    this.selectedSymptoms = this.dummySymptoms[this.selectedDate];
+  }
+
+  onDateChange($event){
+    let ertek = $event.detail.value.split("T")[0]
+    console.log(ertek);
+    this.selectedDate = ertek;
+    this.selectedSymptoms = this.dummySymptoms[ertek];
+    console.log(this.selectedSymptoms);
+    
   }
 
 }
