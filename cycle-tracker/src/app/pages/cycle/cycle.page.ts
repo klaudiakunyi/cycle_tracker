205f9b438fda24e42a37322596f39bb7787ab00e
Kunyi Klaudia <h047786@stud.u-szeged.hu>
2023-02-24 21:53:32 +0100

    cycle page refactor

--- cycle-tracker/src/app/pages/cycle/cycle.page.ts
+++ cycle-tracker/src/app/pages/cycle/cycle.page.ts
@@ -28,48 +28,15 @@ export class CyclePage implements OnInit {
   }
   
   ngOnInit(): void {
-    let foundLastDay = false;
-    let foundfirstDayOfLastPeriod = false;
+
     this.authService.isUserLoggedIn().subscribe(user =>{
       this.userId = user.uid;
       console.log(this.userId);
       this.symptomsService.getSymptomsByUserIdDescendingByDate(this.userId).subscribe(res =>{
         this.symptoms = res;
-        this.firstBleedingDays = [];
-        for(let i = 0; i < this.symptoms.length; i++){
-          if(this.symptoms[i].blood !== 'nothing' && foundLastDay === false){
-            this.lastBleedingDay = this.symptoms[i].date;
-            foundLastDay = true;
-          }
-          let date = DateTime.fromISO(this.symptoms[i].date);
-          let dateBefore = date.minus({days: 1});
-          let dateISO = this.symptoms[i].date;
-          let dateBeforeISO = dateBefore.toISODate();
+        this.getfirstBleedingDays();
+        this.getAverageCycleLength();
 
-          if(this.symptoms[i].blood !== 'nothing' && 
-              (dateISO === dateBeforeISO && this.symptoms[i+1]?.blood === 'nothing') || 
-              this.symptoms[i+1]?.date !== dateBeforeISO ||
-              i >= this.symptoms.length){
-            if(!foundfirstDayOfLastPeriod){
-              this.lastPeriodsFirstBleedingDay = this.symptoms[i].date;
-              foundfirstDayOfLastPeriod = true;
-            }
-            this.firstBleedingDays.push(this.symptoms[i].date)
-            //console.log(this.firstBleedingDays)
-          }
-        }
-        this.diffsInDays = 0;
-        for(let i = 0; i < this.firstBleedingDays.length; i++){
-          if(this.firstBleedingDays[i+1]){
-            console.log(this.firstBleedingDays[i+1])
-            let end = DateTime.fromISO(this.firstBleedingDays[i]);
-            let start = DateTime.fromISO(this.firstBleedingDays[i+1]);
-            let diffInDays = end.diff(start, 'days').days;
-            this.diffsInDays += diffInDays;
-          }
-        }
-        this.averageCycleLength = this.diffsInDays/this.firstBleedingDays.length;
-        console.log("átlagos ciklushossz:" + this.averageCycleLength)
         let nextCycleStartsDate = DateTime.fromISO(this.lastPeriodsFirstBleedingDay).plus({days: this.averageCycleLength})
         this.nextCycleStarts = nextCycleStartsDate.toISODate();
         let fertileWindowLastDate = nextCycleStartsDate.minus({ days: this.fertilityBeforeNextPeriod});
@@ -80,4 +47,44 @@ export class CyclePage implements OnInit {
     })
   }
 
+  getfirstBleedingDays(){
+    let foundLastDay = false;
+    let foundfirstDayOfLastPeriod = false;
+    this.firstBleedingDays = [];
+    for(let i = 0; i < this.symptoms.length; i++){
+      if(this.symptoms[i].blood !== 'nothing' && foundLastDay === false){
+        this.lastBleedingDay = this.symptoms[i].date;
+        foundLastDay = true;
+      }
+      let date = DateTime.fromISO(this.symptoms[i].date);
+      let dateBefore = date.minus({days: 1});
+      let dateISO = this.symptoms[i].date;
+      let dateBeforeISO = dateBefore.toISODate();
+
+      if(this.symptoms[i].blood !== 'nothing' && 
+          (dateISO === dateBeforeISO && this.symptoms[i+1]?.blood === 'nothing') || 
+          this.symptoms[i+1]?.date !== dateBeforeISO ||
+          i >= this.symptoms.length){
+        if(!foundfirstDayOfLastPeriod){
+          this.lastPeriodsFirstBleedingDay = this.symptoms[i].date;
+          foundfirstDayOfLastPeriod = true;
+        }
+        this.firstBleedingDays.push(this.symptoms[i].date)
+      }
+    }
+  }
+
+  getAverageCycleLength(){
+    this.diffsInDays = 0;
+    for(let i = 0; i < this.firstBleedingDays.length; i++){
+      if(this.firstBleedingDays[i+1]){
+        console.log(this.firstBleedingDays[i+1])
+        let end = DateTime.fromISO(this.firstBleedingDays[i]);
+        let start = DateTime.fromISO(this.firstBleedingDays[i+1]);
+        let diffInDays = end.diff(start, 'days').days;
+        this.diffsInDays += diffInDays;
+      }
+    }
+    this.averageCycleLength = this.diffsInDays/this.firstBleedingDays.length;
+  }
 }
